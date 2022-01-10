fb662b258415f89c7338124a06d84f52b2b727e4
aorich <rich.aidan@gmail.com>
2022-01-10 17:52:47 -0500

    some improvements to the project cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import ProfileList from '../components/ProfileList';
-import ProjectList from '../components/ProjectList';
+// import ProfileList from '../components/ProfileList';
+// import ProjectList from '../components/ProjectList';
 
 import { QUERY_MY_PROJECTS } from '../utils/queries';
 
@@ -31,21 +31,19 @@ const Home = () => {
           </Link>
           <div>
             {projects.map((project) =>
+            <div className="card bg-dark mb-3">
               <h4 className="card-header bg-dark text-light p-2 m-0">
-                {project.title} <br />
+                Title: {project.title} <br />
               </h4>
+              <div className="text-light bg-dark p-2 m-0" style={{ fontSize: '1rem' }}>
+                Word Count: {project.wordCount}
+              </div>
+              <div className="text-light bg-dark p-2 m-0" style={{ fontSize: '1rem' }}>
+                Start Date: {project.startDate}
+              </div>
+              </div>
             )}
           </div>
-          {/* <div className="col-12 col-md-10 my-3">
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ProjectList
-                projects={projects}
-                title="Here's your projects..."
-              />
-            )}
-          </div> */}
         </div>
       ) : (
         <h2>Please login.</h2>
